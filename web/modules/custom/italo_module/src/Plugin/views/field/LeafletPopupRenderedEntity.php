61e846afd0aa81ad44f448ed0ee7f26bd6914f9b
itamair <itamair@me.com>
2024-04-23 19:37:01 +0200

    app update

--- web/modules/custom/italo_module/src/Plugin/views/field/LeafletPopupRenderedEntity.php
+++ web/modules/custom/italo_module/src/Plugin/views/field/LeafletPopupRenderedEntity.php
@@ -21,22 +21,24 @@ class LeafletPopupRenderedEntity extends RenderedEntity implements CacheableDepe
    * {@inheritdoc}
    */
   public function render(ResultRow $values) {
-    $entity = Node::load($values->nid);
+    $entity_id = $values->nid;
+    $entity = Node::load($entity_id);
     $components = $entity->get('field_components')->getValue();
     $entity = $this->getEntityTranslationByRelationship($entity, $values);
-    $paragraph_id = $values->paragraphs_item_field_data_node__field_components_id_1;
-    $paragraph = Paragraph::load($paragraph_id);
     $new_entity = clone($entity);
-    if ($paragraph->bundle() == 'geoimage') {
-      $paragraph_id_component = [];
-      foreach ($components as $component) {
-        if ($component['target_id'] === $paragraph_id) {
-          $paragraph_id_component = $component;
-          break;
+    if ($paragraph_id = $values->paragraphs_item_field_data_node__field_components_id_1 ?? NULL) {
+      $paragraph = Paragraph::load($paragraph_id);
+      if ($paragraph->bundle() == 'geoimage') {
+        $paragraph_id_component = [];
+        foreach ($components as $component) {
+          if ($component['target_id'] === $paragraph_id) {
+            $paragraph_id_component = $component;
+            break;
+          }
         }
-      }
-      if (!empty($paragraph_id_component) && $entity->bundle() === 'territorial_report') {
-        $new_entity = $new_entity->set('field_components', $paragraph_id_component);
+        if (!empty($paragraph_id_component) && $entity->bundle() === 'territorial_report') {
+          $new_entity = $new_entity->set('field_components', $paragraph_id_component);
+        }
       }
     }
     $build = [];
@@ -46,14 +48,14 @@ class LeafletPopupRenderedEntity extends RenderedEntity implements CacheableDepe
       $view_builder = $this->entityTypeManager->getViewBuilder($this->getEntityTypeId());
       $build += $view_builder->view($new_entity, $this->options['view_mode'], $new_entity->language()->getId());
     }
-/*    $build["#cache"]["tags"] = [
-      'paragraph:' . $paragraph_id,
-    ];*/
+    /*    $build["#cache"]["tags"] = [
+          'paragraph:' . $paragraph_id,
+        ];*/
     $build["#cache"]["keys"] = [
       "entity_view",
       "paragraph",
       "leaflet_popup",
-      $paragraph_id,
+      $paragraph_id ?? $entity_id,
     ];
     return $build;
   }
