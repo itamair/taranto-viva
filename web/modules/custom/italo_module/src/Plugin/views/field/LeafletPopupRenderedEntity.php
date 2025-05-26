334e2cf5e4ac3ccec10653ce217768a70702a96e
itamair <itamair@me.com>
2025-05-26 10:52:55 +0200

    LeafletPopupRenderedEntity update

--- web/modules/custom/italo_module/src/Plugin/views/field/LeafletPopupRenderedEntity.php
+++ web/modules/custom/italo_module/src/Plugin/views/field/LeafletPopupRenderedEntity.php
@@ -29,35 +29,38 @@ class LeafletPopupRenderedEntity extends RenderedEntity implements CacheableDepe
       $components = [];
     }
     $entity = $this->getEntityTranslationByRelationship($entity, $values);
-    $paragraph_id = $values->paragraphs_item_field_data_node__field_components_id_1;
-    $paragraph = Paragraph::load($paragraph_id);
-    $new_entity = clone($entity);
-    if ($paragraph->bundle() == 'geoimage') {
-      $paragraph_id_component = [];
-      foreach ($components as $component) {
-        if ($component['target_id'] === $paragraph_id) {
-          $paragraph_id_component = $component;
-          break;
-        }
-      }
-      if (!empty($paragraph_id_component) && $entity->bundle() === 'territorial_report') {
-        $new_entity = $new_entity->set('field_components', $paragraph_id_component);
-      }
-    }
     $build = [];
-    $access = $entity->access('view', NULL, TRUE);
-    $build['#access'] = $access;
-    if ($access->isAllowed()) {
-      $view_builder = $this->entityTypeManager->getViewBuilder($this->getEntityTypeId());
-      $build += $view_builder->view($new_entity, $this->options['view_mode'], $new_entity->language()->getId());
-    }
+    $paragraph_id = $values->paragraphs_item_field_data_node__field_components_id_1 ?? $values->paragraphs_item_field_data_node__field_components_id;
+    if (!empty($paragraph_id)) {
+      $paragraph = Paragraph::load($paragraph_id);
+      $new_entity = clone($entity);
+      if ($paragraph->bundle() == 'geoimage') {
+        $paragraph_id_component = [];
+        foreach ($components as $component) {
+          if ($component['target_id'] === $paragraph_id) {
+            $paragraph_id_component = $component;
+            break;
+          }
+        }
+        if (!empty($paragraph_id_component) && $entity->bundle() === 'territorial_report') {
+          $new_entity = $new_entity->set('field_components', $paragraph_id_component);
+        }
 
-    $build["#cache"]["keys"] = [
-      "entity_view",
-      "paragraph",
-      "leaflet_popup",
-      $paragraph_id,
-    ];
+        $access = $entity->access('view', NULL, TRUE);
+        $build['#access'] = $access;
+        if ($access->isAllowed()) {
+          $view_builder = $this->entityTypeManager->getViewBuilder($this->getEntityTypeId());
+          $build += $view_builder->view($new_entity, $this->options['view_mode'], $new_entity->language()->getId());
+        }
+
+        $build["#cache"]["keys"] = [
+          "entity_view",
+          "paragraph",
+          "leaflet_popup",
+          $paragraph_id,
+        ];
+      }
+    }
     return $build;
   }
 
