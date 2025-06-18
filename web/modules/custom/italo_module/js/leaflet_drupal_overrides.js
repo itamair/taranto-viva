ffba463503fe2d89f58b46b64f2401ed6c36eab9
itamair <itamair@me.com>
2025-06-18 19:28:07 +0200

    Application update with google maps links on specific geometries clicks (popup open)

--- web/modules/custom/italo_module/js/leaflet_drupal_overrides.js
+++ web/modules/custom/italo_module/js/leaflet_drupal_overrides.js
@@ -98,6 +98,27 @@ Drupal.Leaflet.prototype.feature_bind_popup = function(lFeature, feature) {
   if (!parseInt(feature_properties.popup_disabled) && feature.popup && feature.popup.value) {
     const popup_options = feature.popup.options ? JSON.parse(feature.popup.options) : {};
     lFeature.bindPopup(feature.popup.value, popup_options);
+
+    // In case of features not points (linestring, polygons) then add specific
+    // google maps links to its pop content, and remove on close.
+    if (feature.type !== 'point') {
+      lFeature.on('popupopen', function (e) {
+        const popup = e.popup;
+        const lat = popup.getLatLng().lat;
+        const lng = popup.getLatLng().lng;
+        const content = '<div class="field field--name-geofield-googlemaps-link field--type-link field--label-hidden field__items">\n' +
+          '<div class="field__item"><a href="https://www.google.com/maps/search/?api=1&amp;query=' + lat + '%2C' + lng + '" target="_blank">Google Maps</a></div>\n' +
+          '<div class="field__item"><a href="https://www.google.com/maps/@?api=1&amp;map_action=pano&amp;viewpoint=' + lat + '%2C' + lng + '" target="_blank">Street View</a></div>\n' +
+          '</div>';
+        //popup.setContent('Coordinates: ' +  popup.getLatLng().lng + ' / ' + popup.getLatLng().lat);
+        popup.setContent(popup.getContent() + content);
+        console.log(e);
+      });
+      lFeature.on('popupclose', function (e) {
+        const popup = e.popup;
+        popup.setContent(feature.popup.value);
+      });
+    }
   }
 };
 
