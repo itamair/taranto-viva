42f584ba6c60161c4b2aea69297cb26c2d854b7c
itamair <itamair@me.com>
2025-10-15 15:02:25 +0200

    Set up the app to library mode Configure vite.config.ts file to generate the Vite build version of your component library in library mode. @see https://www.acrocommerce.com/article/embed-react-into-drupal-for-better-development-efficiency

--- web/modules/custom/react_maplibre_map/vite.config.ts
+++ web/modules/custom/react_maplibre_map/vite.config.ts
@@ -1,7 +1,40 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+//vite.config.ts
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+import { defineConfig, loadEnv } from 'vite';
+import path from 'path';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [react()],
+    build: {
+      rollupOptions: {
+        output: {
+          entryFileNames: 'index.js',
+          chunkFileNames: 'assets/[name].js',
+          assetFileNames: 'assets/[name].[ext]',
+        },
+      },
+      lib: {
+        entry: path. resolve(__dirname, 'src/main.tsx'),
+        formats: ['es'],
+        name: 'index',
+      },
+    },
+    resolve: {
+      alias: {
+        '~': path.resolve(__dirname, 'src/'),
+      },
+    },
+    define: {
+      ...Object.keys(env).reduce((prev, key) => {
+        prev[`process.env.${key}`] = JSON.stringify(env[key]);
+        return prev;
+      }, {} as Record<string, string>),
+    },
+  }
+
+});
