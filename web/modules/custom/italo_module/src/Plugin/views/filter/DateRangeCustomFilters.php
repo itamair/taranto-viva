0580de5f7f27bc052cd75c750e799b6194844c8b
itamair <itamair@me.com>
2025-12-01 17:14:39 +0100

    tmp View Date DateRangeCustomFilters

--- web/modules/custom/italo_module/src/Plugin/views/filter/DateRangeCustomFilters.php
+++ web/modules/custom/italo_module/src/Plugin/views/filter/DateRangeCustomFilters.php
@@ -3,6 +3,7 @@
 namespace Drupal\italo_module\Plugin\views\filter;
 
 use Drupal\Component\Datetime\DateTimePlus;
+use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\datetime\Plugin\Field\FieldType\DateTimeItemInterface;
 use Drupal\datetime\Plugin\views\filter\Date;
@@ -22,6 +23,42 @@ use Drupal\datetime\Plugin\views\filter\Date;
 class DateRangeCustomFilters extends Date implements ContainerFactoryPluginInterface {
 
   /**
+   * Add a type selector to the value form.
+   */
+  protected function valueForm(&$form, FormStateInterface $form_state) {
+    if (!$form_state->get('exposed')) {
+      $form['value']['type'] = [
+        '#type' => 'radios',
+        '#title' => $this->t('Value type'),
+        '#options' => [
+          'date' => $this->t('A date in any machine readable format. CCYY-MM-DD HH:MM:SS is preferred.'),
+          'offset' => $this->t('An offset from the current time such as "@example1" or "@example2"', [
+            '@example1' => '+1 day',
+            '@example2' => '-2 hours -30 minutes',
+          ]
+          ),
+        ],
+        '#default_value' => !empty($this->value['type']) ? $this->value['type'] : 'date',
+      ];
+    }
+    parent::valueForm($form, $form_state);
+
+    $form["value"] = [
+      '#type' => 'select',
+      '#title' => $this->t('Hidden Layers Controls'),
+      '#description' => $this->t('Choose the Layers that will not appear in the Layers Control'),
+      '#options' => [
+        '-1 month' => $this->t('1 Month Ago'),
+        '+1 day' => $this->t('Tomorrow'),
+        '+1 week' => $this->t('In 1 Week'),
+        '+1 month' => $this->t('In 1 Month'),
+      ],
+      '#default_value' => '+1 day',
+    ];
+
+  }
+
+  /**
    * {@inheritdoc}
    *
    * @return array
@@ -34,6 +71,12 @@ class DateRangeCustomFilters extends Date implements ContainerFactoryPluginInter
       'method' => 'opDateRangeIncludes',
       'short' => $this->t('dr_includes'),
       'values' => 1,
+    ];
+    $operators['date_range_overlaps'] = [
+      'title' => $this->t('Date Range Overlaps'),
+      'method' => 'opDateRangeOverlaps',
+      'short' => $this->t('dr_within'),
+      'values' => 2,
     ];
     return $operators;
   }
@@ -58,6 +101,34 @@ class DateRangeCustomFilters extends Date implements ContainerFactoryPluginInter
     $end_field = $this->query->getDateFormat($this->query->getDateField($end_field, TRUE, $this->calculateOffset), $this->dateFormat, TRUE);
 
     $this->query->addWhereExpression($this->options['group'], "($end_field IS NULL AND $value >= $field) OR $value BETWEEN $field AND $end_field");
+  }
+
+  /**
+   * Filters by operator Overlaps.
+   *
+   * @param object $field
+   *   The views field.
+   */
+  protected function opDateRangeOverlaps($field) {
+    $end_field = substr($field, 0, -6) . '_end_value';
+
+    $timezone = $this->getTimezone();
+    $origin_offset = $this->getOffset($this->value['min'], $timezone);
+
+    // Although both 'min' and 'max' values are required, default empty 'min'
+    // value as UNIX timestamp 0.
+    $min = (!empty($this->value['min'])) ? $this->value['min'] : '@0';
+
+    // Convert to ISO format and format for query. UTC timezone is used since
+    // dates are stored in UTC.
+    $a = new DateTimePlus($min, new \DateTimeZone($timezone));
+    $a = $this->query->getDateFormat($this->query->getDateField("'" . $this->dateFormatter->format($a->getTimestamp() + $origin_offset, 'custom', DateTimeItemInterface::DATETIME_STORAGE_FORMAT, DateTimeItemInterface::STORAGE_TIMEZONE) . "'", TRUE, $this->calculateOffset), $this->dateFormat, TRUE);
+    $b = new DateTimePlus($this->value['max'], new \DateTimeZone($timezone));
+    $b = $this->query->getDateFormat($this->query->getDateField("'" . $this->dateFormatter->format($b->getTimestamp() + $origin_offset, 'custom', DateTimeItemInterface::DATETIME_STORAGE_FORMAT, DateTimeItemInterface::STORAGE_TIMEZONE) . "'", TRUE, $this->calculateOffset), $this->dateFormat, TRUE);
+
+    $field = $this->query->getDateFormat($this->query->getDateField($field, TRUE, $this->calculateOffset), $this->dateFormat, TRUE);
+    $end_field = $this->query->getDateFormat($this->query->getDateField($end_field, TRUE, $this->calculateOffset), $this->dateFormat, TRUE);
+    $this->query->addWhereExpression($this->options['group'], "($a <= $end_field AND $b >= $field) OR ($a <= $field AND $b >= $field)");
   }
 
 }
