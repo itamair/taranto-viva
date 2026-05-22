a8b49426614f00d9c19e00c97676699df7f08b99
itamair <itamair@me.com>
2026-05-22 17:09:58 +0200

    leaflet_drupal_overrides.js: defined parametric absoluteMarkerclusterIconsPath

--- public_html/modules/custom/italo_module/js/leaflet_drupal_overrides.js
+++ public_html/modules/custom/italo_module/js/leaflet_drupal_overrides.js
@@ -281,11 +281,15 @@
    */
   Drupal.Leaflet.prototype.add_features = function (features, initial) {
 
+    const absoluteBaseUrl = window.location.origin + drupalSettings.path.baseUrl;
+    const markerclusterIconsPath = 'sites/default/files/markercluster_icons/';
+    const absoluteMarkerclusterIconsPath = absoluteBaseUrl + markerclusterIconsPath;
+
     const svg_icons = {
-      '2': '<img src="https://www.taranto-viva.com/sites/default/files/markercluster_icons/marker-cluster_orange_2.svg" style="width:30px; height:30px; margin-top:-10px; margin-left:-10px">',
-      '3': '<img src="https://www.taranto-viva.com/sites/default/files/markercluster_icons/marker-cluster_orange_3.svg" style="width:30px; height:30px; margin-top:-10px; margin-left:-10px">',
-      '5': '<img src="https://www.taranto-viva.com/sites/default/files/markercluster_icons/marker-cluster_orange_5.svg" style="width:30px; height:30px; margin-top:-10px; margin-left:-10px">',
-      '8': '<img src="https://www.taranto-viva.com/sites/default/files/markercluster_icons/marker-cluster_orange_8.svg" style="width:30px; height:30px; margin-top:-10px; margin-left:-10px">',
+      '2': '<img src="' + absoluteMarkerclusterIconsPath + 'marker-cluster_orange_2.svg" style="width:30px; height:30px; margin-top:-10px; margin-left:-10px">',
+      '3': '<img src="' + absoluteMarkerclusterIconsPath + 'marker-cluster_orange_3.svg" style="width:30px; height:30px; margin-top:-10px; margin-left:-10px">',
+      '5': '<img src="' + absoluteMarkerclusterIconsPath + 'marker-cluster_orange_5.svg" style="width:30px; height:30px; margin-top:-10px; margin-left:-10px">',
+      '8': '<img src="' + absoluteMarkerclusterIconsPath + 'marker-cluster_orange_8.svg" style="width:30px; height:30px; margin-top:-10px; margin-left:-10px">',
     }
 
     let leaflet_markercluster_options = this.map_settings.leaflet_markercluster.options && this.map_settings.leaflet_markercluster.options.length > 0 ? JSON.parse(this.map_settings.leaflet_markercluster.options) : {};
